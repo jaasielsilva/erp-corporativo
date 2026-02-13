b7cc3a6c0fa3f1e3d44609e95eb737f716ffa8b3
jaasielsilva <jasiel1@outlook.com>
2026-02-13 10:41:47 -0300

    app estavel

---
+++ src/main/java/com/jaasielsilva/portalceo/PortalCeoApplication.java
@@ -0,0 +1,17 @@
+package com.jaasielsilva.portalceo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
+public class PortalCeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalCeoApplication.class, args);
+    }
+}
